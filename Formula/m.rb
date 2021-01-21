a63edbe3aececde422904e1008ea233b50e42fc1
Andreas Braun <git@alcaeus.org>
2021-01-21 09:16:47 +0100

    Update to latest m version

--- Formula/m.rb
+++ Formula/m.rb
@@ -1,9 +1,9 @@
 class M < Formula
   desc "Helps you download, use, and manage multiple versions of the MongoDB server and command-line tools"
   homepage "https://github.com/aheckmann/m"
-  url "https://raw.githubusercontent.com/aheckmann/m/1.5.4/bin/m"
-  version "1.5.4"
-  sha256 "5c2cc3c8d4ac1390db1fc4ddd0da110061e2a349662a755dff0fc0fe493d7a21"
+  url "https://raw.githubusercontent.com/aheckmann/m/1.5.6/bin/m"
+  version "1.5.6"
+  sha256 "a23de33ce8a0ad1cb83e93d5a02cf76f70eef6b107adf287a149df1e0ecc521b"
 
   bottle :unneeded
 

33cdd99ec81b03302fd4e00bab4ef310a8d9e583
Andreas Braun <git@alcaeus.org>
2023-02-07 10:21:24 +0100

    m 1.8.4

--- Formula/m.rb
+++ Formula/m.rb
@@ -1,11 +1,9 @@
 class M < Formula
   desc "Helps you download, use, and manage multiple versions of the MongoDB server and command-line tools"
   homepage "https://github.com/aheckmann/m"
-  url "https://raw.githubusercontent.com/aheckmann/m/1.6.0/bin/m"
-  version "1.6.0"
-  sha256 "7b21ca57f2d7442abc18827f3ad2308258fc8cbc06e234bfd98ea7a458ff5e66"
-
-  bottle :unneeded
+  url "https://raw.githubusercontent.com/aheckmann/m/1.8.4/bin/m"
+  version "1.8.4"
+  sha256 "ba1ece9f49e261ad97eb1b343c7d6c968a003eff5d834d0b7a533f2d64a709c1"
 
   def install
     bin.install "m"
